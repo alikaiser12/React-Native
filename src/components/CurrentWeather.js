33579e3a992e3603eb68261df9effc4831acbbc0
Ali Kaiser <rimu.alikaiser@gmail.com>
2024-06-10 20:22:59 +0600

    First Commit

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { View,Text, SafeAreaView, StyleSheet} from "react-native";
+import {Feather} from '@expo/vector-icons';
+const CurrentWeather = () =>{
+
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black"/>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels Like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.hoghLow}>High: 8</Text>
+          <Text style={styles.hoghLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+          <Text style={styles.description}> Its sunny</Text>
+          <Text style={styles.message}>Its perfect t-shirt weather</Text>
+      </View>
+        
+      
+    </SafeAreaView>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper:{
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  temp:{
+    color: 'black',
+    fontSize: 48
+  },
+  feels:{
+    fontSize:30,
+    color: 'black'
+  },
+  hoghLow:{
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper:{
+    flexDirection:'row'
+  },
+  bodyWrapper:{
+    justifyContent:'flex-end',
+    alignItems:'flex-start',
+    paddingLeft: 25,
+    marginBottom:40
+  },
+  description:{
+    fontSize: 48
+  },
+  message:{
+    fontSize:30
+  }
+})
+
+export default CurrentWeather;
